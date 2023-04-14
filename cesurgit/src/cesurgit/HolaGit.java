e78c1babceed256790934bf80c0c73ce53a0eaed
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-04-14 21:44:19 +0200

    listo

--- cesurgit/src/cesurgit/HolaGit.java
+++ cesurgit/src/cesurgit/HolaGit.java
@@ -5,7 +5,10 @@ public class HolaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("Hola");
+		System.out.print("Hola, ");
+		System.out.print("este es el ejercicio no evaluable");
+		System.out.print("del mes de marzo, ");
+		System.out.print("para la asignatura de Entornos de Desarrollo");
 		
 	}
 
